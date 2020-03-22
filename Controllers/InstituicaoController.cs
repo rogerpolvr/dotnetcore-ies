71590e17990cac9eeded92aaed74f8db3366d910
Roger Oliveira <rogerpolvr@gmail.com>
2020-03-22 00:42:25 -0300

    Criando a solução IES

---
+++ Controllers/InstituicaoController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IES.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IES.Controllers
+{
+    public class InstituicaoController : Controller
+    {
+        private static IList<Instituicao> instituicoes = new List<Instituicao>()
+        {
+            new Instituicao()
+            {
+                InstituicaoID = 1,
+                Nome = "PUCRS",
+                Endereco = "Av Ipiranga - Porto Alegre"
+            },
+            new Instituicao()
+            {
+                InstituicaoID = 1,
+                Nome = "Unisinos",
+                Endereco = "Av Unisinos - São Leopoldo"
+            },
+            new Instituicao()
+            {
+                InstituicaoID = 1,
+                Nome = "Ulbra",
+                Endereco = "Av Ulbra - Canoas"
+            }
+        };
+        public IActionResult Index()
+        {
+            return View(instituicoes);
+        }
+
+
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IES.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
