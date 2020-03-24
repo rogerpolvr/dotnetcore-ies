197f736fe6226f4fa2e5a529b8e6d1591e99cedc
Roger Oliveira <rogerpolvr@gmail.com>
2020-03-23 21:34:29 -0300

    Implementando o CRUD do modelo Instituição

--- Controllers/InstituicaoController.cs
+++ Controllers/InstituicaoController.cs
@@ -19,20 +19,75 @@ namespace IES.Controllers
             },
             new Instituicao()
             {
-                InstituicaoID = 1,
+                InstituicaoID = 2,
                 Nome = "Unisinos",
                 Endereco = "Av Unisinos - São Leopoldo"
             },
             new Instituicao()
             {
-                InstituicaoID = 1,
+                InstituicaoID = 3,
                 Nome = "Ulbra",
                 Endereco = "Av Ulbra - Canoas"
             }
         };
+
+        //GET: Create
         public IActionResult Index()
         {
-            return View(instituicoes);
+            return View(instituicoes.OrderBy(i => i.InstituicaoID));
+        }
+
+        //GET: Create
+        public ActionResult Create()
+        {
+            return View();
+        }  
+        
+        //POST: Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Instituicao instituicao)
+        {
+            instituicoes.Add(instituicao);
+            instituicao.InstituicaoID = instituicoes.Select(i => i.InstituicaoID).Max() + 1;
+            return RedirectToAction("Index");
+        }
+
+        //GET: Edit
+        public ActionResult Edit(long id)
+        {
+            return View(instituicoes.Where(i => i.InstituicaoID == id).First());
+        }
+
+        //POST: Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Instituicao instituicao)
+        {
+            instituicoes.Remove(instituicoes.Where(i => i.InstituicaoID == instituicao.InstituicaoID).First());
+            instituicoes.Add(instituicao);
+            return RedirectToAction("Index");
+        }
+
+        //GET: Details
+        public ActionResult Details(long id)
+        {
+            return View(instituicoes.Where(i => i.InstituicaoID == id).First());
+        }
+
+        //GET: Delete
+        public ActionResult Delete(long id)
+        {
+            return View(instituicoes.Where(i => i.InstituicaoID == id).First());
+        }
+
+        //POST: Delete
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(Instituicao instituicao)
+        {
+            instituicoes.Remove(instituicoes.Where(i => i.InstituicaoID == instituicao.InstituicaoID).First());
+            return RedirectToAction("Index");
         }
 
 
