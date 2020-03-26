4b0de5ecc34293484dc77f31142c13bf85ca2af1
Roger Oliveira <rogerpolvr@gmail.com>
2020-03-25 21:53:25 -0300

    Criando o IESContext e a Model Departamento

---
+++ Data/IESDbInitializer.cs
@@ -0,0 +1,33 @@
+﻿using IES.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IES.Data
+{
+    public class IESDbInitializer
+    {
+        public static void Initialize(IESContext context)
+        {
+            context.Database.EnsureCreated();
+
+            if (context.Departamentos.Any())
+            {
+                return;
+            }
+
+            var departamentos = new Departamento[]
+            {
+                new Departamento { Nome="Ciência da Computação"},
+                new Departamento { Nome="Ciência de Alimentos"}
+            };
+
+            foreach (Departamento d in departamentos)
+            {
+                context.Departamentos.Add(d);
+            }
+            context.SaveChanges();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,22 +1,40 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using IES.Data;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace IES {
-    public class Program {
-        public static void Main (string[] args) {
-            CreateHostBuilder (args).Build ().Run ();
+
+
+namespace IES
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateWebHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<IESContext>();
+                    IESDbInitializer.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "Um erro ocorreu ao popular a base de dados.");
+                }
+            }
+
+            host.Run();
         }
 
-        public static IHostBuilder CreateHostBuilder (string[] args) =>
-            Host.CreateDefaultBuilder (args)
-            .ConfigureWebHostDefaults (webBuilder => {
-                webBuilder.UseStartup<Startup> ();
-            });
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
     }
 }
