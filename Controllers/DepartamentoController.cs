df92150ead9fd7aa5669a680aaace5975629413e
Roger Oliveira <rogerpolvr@gmail.com>
2020-03-28 22:33:18 -0300

    Criano o DepartamentoController e as Views Index e Create utilizando o database context

---
+++ Controllers/DepartamentoController.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IES.Data;
+using IES.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace IES
+{
+    public class DepartamentoController : Controller
+    {
+        private readonly IESContext _context;
+
+        public DepartamentoController(IESContext context)
+        {
+            this._context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Departamentos.OrderBy(d => d.Nome).ToListAsync());
+        }
+
+        //GET: Departamento/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Nome")] Departamento departamento)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _context.Add(departamento);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            catch (DbUpdateException)
+            {
+                ModelState.AddModelError("", "Não foi possível inserir os dados.");
+            }
+
+            return View(departamento);
+        }
+    }
+}
