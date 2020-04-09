70e22e343833fe5d79bc800c72f38e5ebdaa014b
Roger Oliveira <rogerpolvr@gmail.com>
2020-04-09 20:46:54 -0300

    Implementando as classes Curso e Disciplina do Modelo

---
+++ Model/Cadastros/CursoDisciplina.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model.Cadastros
+{
+    public class CursoDisciplina
+    {
+        public long? CursoID { get; set; }
+        public Curso Curso { get; set; }
+        public long? DisciplinaID { get; set; }
+        public Disciplina Disciplina { get; set; }
+    }
+}
+
---
+++ Model/Cadastros/Disciplina.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model.Cadastros
+{
+    public class Disciplina
+    {
+        public long? CursoID { get; set; }
+        public Curso Curso { get; set; }
+    }
+}
