b8494930160856f1c7e70b026b98bda0e510e3b9
Roger Oliveira <rogerpolvr@gmail.com>
2020-04-10 22:08:45 -0300

    Criando o modelo para Academico

---
+++ Model/Discente/Academico.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Model.Discente
+{
+    public class Academico
+    {
+        public long? AcademicoID { get; set; }
+
+        [StringLength(10, MinimumLength = 10)]
+        [RegularExpression("([0-9]{10}")]
+        [Required]
+        public string RegistroAcademico { get; set; }
+
+        [Required]
+        public string Nome { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0: dd-MM-yyyy}")]
+        [Required]
+        public DateTime? Nascimento { get; set; }
+
+    }
+}
